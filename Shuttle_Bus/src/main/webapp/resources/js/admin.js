de45cb81eaf3594f338bc97f7c838c5d6fde9dda
MaiMom <maimom15@kit.edu.kh>
2017-07-11 07:45:22 -0700

    new

---
+++ Shuttle_Bus/src/main/webapp/resources/js/admin.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+    $('.modal').modal();
+    $(".button-collapse").sideNav();
+    $('#modal2').modal();
+    $('select').material_select();
+    $('#modal3').modal();
+  });
