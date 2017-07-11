be488b631467488933383b59793340dc13e70034
varathana12 <varathana15@kit.edu.kh>
2017-07-11 06:24:24 -0400

    ff

--- Shuttle_Bus/src/main/java/com/DaoClasses/Admin_Imp.java
+++ Shuttle_Bus/src/main/java/com/DaoClasses/Admin_Imp.java
@@ -1,7 +1,42 @@
 package com.DaoClasses;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.EntityClasses.Destination_Master;
+import com.EntityClasses.Passenger;
+import com.EntityClasses.Schedule_Table;
+
 public class Admin_Imp implements Admin_Inf{
 
+	public List<Destination_Master> Booking_API() {
+        List<Destination_Master> dest = new ArrayList<Destination_Master>();
+        Transaction trns = null;
+        Configuration con=new Configuration();
+        con.configure("hibernate.cfg.xml");
+     	SessionFactory sf=con.buildSessionFactory();
+     	Session session=sf.openSession();
+        
+        try {
+            trns = session.beginTransaction();
+            String hql ="FROM Destination_Master as dest INNER JOIN dest.Passenger";
+            Query query =  session.createQuery(hql);
+            dest = query.list();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            return dest;
+        } finally {
+            session.flush();
+            session.close();
+        }
+        return dest;
+    }
 	
 	
 }
--- Shuttle_Bus/src/main/java/com/MainController/ControllerFile1.java
+++ Shuttle_Bus/src/main/java/com/MainController/ControllerFile1.java
@@ -1,7 +1,10 @@
 package com.MainController;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.DaoClasses.Admin_Imp;
 import com.DaoClasses.TeacherDao;
 import com.DaoClasses.Teacher_Implement;
 import com.EntityClasses.Authentic;
@@ -36,6 +40,28 @@ public class ControllerFile1{
 		return view;
 		
 	}
+
+	@RequestMapping(value="/bookingPage",method = RequestMethod.GET)
+		public @ResponseBody ArrayList<Map> bookingPage(){
+			
+			List<Destination_Master> destlist = new ArrayList<Destination_Master>();
+			Admin_Imp dest = new Admin_Imp();
+			destlist = dest.Booking_API();
+			Map <String,Object> join = new HashMap<String,Object>();
+			ArrayList<Map> bookingapi=new ArrayList<Map>();  
+		
+			for(int i=0;i<destlist.size();i++){
+				Map <String,Object> pass = new HashMap<String,Object>();
+				join.put("id",destlist.get(i).getDestination_id());
+				join.put("name", destlist.get(i).getDestination_name());
+				join.put("departure", destlist.get(i).getDeparture_time());
+				//pass.put("pass", destlist.get(i).getPassenger().);
+				
+				bookingapi.add(join);
+				
+			}
+			return bookingapi;
+		}
 	
 	
 	
